591d6fd2edb2f60bb1ce1d99957c5521a02e2481
Isa idarraga <iidarragab@eafit.edu.co>
2024-05-09 12:00:35 -0500

    se agrego codigo

---
+++ main.java
@@ -0,0 +1,8 @@
+public class main{
+    public static void main(String[] args) {
+        nombre();
+    }
+    public static nombre(){
+        System.out.println("Assignment 3 by Isabella Idarraga");
+    }
+}
